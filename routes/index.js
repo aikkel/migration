cf3a85adb2ecd9b3621758eb4fc1288ac48636cb
aikkel <mikk39k8@edu.zealand.dk>
2023-11-10 10:37:09 +0100

    added forntpage for sendNemt

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var sqlite3 = require('sqlite3').verbose();
 
 var db = require('../models');
 
- var users = [
+var users = [
   {"name": "Oscar",
   "email": "oscar@noob.tk",
   "phone": "123456789"},
@@ -16,17 +16,17 @@ var db = require('../models');
   "phone": "123456789"}
 ]
 
-
-
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var users = db.User.findAll().then(users => {
     console.log(users);
     res.render('index', { title: 'Brugere i systemet', users });
   });
-  
+});
+
+/* GET frontpage. */
+router.get('/frontpage', function(req, res, next) {
+  res.render('frontpage', { title: 'Frontpage' });
 });
 
 module.exports = router;
