49550d572f2866d6c57195d0bdbd49580c76c881
Narenboi <Luhavenkvist@yahoo.dk>
2023-11-13 12:42:02 +0100

    Masse opdateringer. Vi kan nu indsætte brugere i databasen ved tryk på en knap

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,70 @@
 var express = require('express');
 var router = express.Router();
-var sqlite3 = require('sqlite3').verbose();
-
 var db = require('../models');
-
-var users = [
-  {"name": "Oscar",
-  "email": "oscar@noob.tk",
-  "phone": "123456789"},
-  {"name": "Bob",
-  "email": "bobo@bob.bob",
-  "phone": "987654321"},
-  {"name": "Alice",
-  "email": "po@pod.dk",
-  "phone": "123456789"}
-]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var users = db.User.findAll().then(users => {
+  db.User.findAll().then(users => {
     console.log(users);
-    res.render('index', { title: 'Brugere i systemet', users });
+    res.render('frontpage', { title: 'Brugere i systemet', users });
   });
 });
 
-/* GET frontpage. */
-router.get('/frontpage', function(req, res, next) {
-  res.render('frontpage', { title: 'Frontpage' });
+/* GET home page. */
+router.get('/data', function(req, res, next) {
+  db.User.findAll().then(users => {
+    console.log(users);
+    res.render('databaseSearch', { title: 'Database Print Out', users });
+  });
 });
+
+/* POST route for inserting user data */
+router.post('/insertUser', function(req, res, next) {
+  // Extract data from the request body
+  const {
+    senderName,
+    senderEmail,
+    senderPhone,
+    senderAddress,
+    receiverName,
+    receiverEmail,
+    receiverPhone,
+    receiverAddress,
+    parcelWeight,
+    parcelDimensions,
+    parcelContents,
+    parcelValue,
+  } = req.body;
+
+  // You can add validation or further processing here if needed
+
+  // Create a new user in the database using the User model
+  db.User.create({
+    senderName,
+    senderEmail,
+    senderPhone,
+    senderAddress,
+    receiverName,
+    receiverEmail,
+    receiverPhone,
+    receiverAddress,
+    parcelWeight,
+    parcelDimensions,
+    parcelContents,
+    parcelValue,
+  })
+    .then(newUser => {
+      // Handle successful insertion
+      console.log('User inserted:', newUser);
+      res.redirect('/data'); // Redirect to the data page or any other page you prefer
+    })
+    .catch(error => {
+      // Handle insertion error
+      console.error('Error inserting user:', error);
+      res.status(500).json({ error: 'Failed to insert user' });
+    });
+});
+
+
 
 module.exports = router;
