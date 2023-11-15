76d8a8a8828e47acd32c22ebd66e66d88e2e0f98
Narenboi <Luhavenkvist@yahoo.dk>
2023-11-15 13:22:25 +0100

    Prøvet at få programmet til at køre, der bliver dog ved med at være errors.
    Opdateret nogle database kald i koden.

--- models/DatabaseSearch.js
+++ models/DatabaseSearch.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sequelize = require('./db'); // Import your database configuration
-const User = require('./models/user'); // Import the User model
+const { Sequelize } = require('sequelize');
+const Sender = require('./Sender.js'); // Corrected path
+const Receiver = require('./Receiver.js'); // Corrected path
 const app = express();
 
 app.set('view engine', 'pug');
@@ -9,44 +10,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Render the form to input data
 app.get('/data', (req, res) => {
-  res.render('databaseSearch', { title: 'Database Search', users: [] });
+  res.render('databaseSearch', { title: 'Database Search', Sender: [] });
 });
-// Handle the form submission and insert data into the User table
-app.post('/insertUser', async (req, res) => {
-  try {
-    const {
-      senderName,
-      senderEmail,
-      senderPhone,
-      senderAddress,
-      receiverName,
-      receiverEmail,
-      receiverPhone,
-      receiverAddress,
-      parcelWeight,
-      parcelDimensions,
-      parcelContents,
-      parcelValue,
-    } = req.body; // Extract data from the form fields
 
-    const newUser = await User.create({
-      senderName,
-      senderEmail,
-      senderPhone,
-      senderAddress,
-      receiverName,
-      receiverEmail,
-      receiverPhone,
-      receiverAddress,
-      parcelWeight,
-      parcelDimensions,
-      parcelContents,
-      parcelValue,
+// Handle the form submission and insert data into the User table for sender
+app.post('/insertSender', async (req, res) => {
+  try {
+    const { FirstName, LastName, Email, Phonenumber, Address } = req.body;
+    const newSender = await Sender.create({
+      firstName: FirstName,
+      lastName: LastName,
+      email: Email,
+      phoneNumber: Phonenumber,
+      address: Address,
     });
 
-    res.json(newUser);
+    res.json(newSender);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to insert user' });
+    res.status(500).json({ error: 'Failed to insert sender' });
   }
+});
+
+// Handle the form submission and insert data into the User table for receiver
+app.post('/insertReceiver', async (req, res) => {
+  try {
+    const { FirstName, LastName, Email, Phonenumber, Address } = req.body;
+    const newReceiver = await Receiver.create({
+      firstName: FirstName,
+      lastName: LastName,
+      email: Email,
+      phoneNumber: Phonenumber,
+      address: Address,
+    });
+
+    res.json(newReceiver);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to insert receiver' });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
