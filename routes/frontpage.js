5908db7019001079fa9f3f7fea8e5a7fe8f36632
Naren's bumcrack <luhavenkvist@yahoo.dk>
2023-11-17 12:01:58 +0100

    Vi kan nu få adgang til start siden dog uden css

--- routes/frontpage.js
+++ routes/frontpage.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Sender } = require('../models');
 
-router.post('/insertSender', async (req, res) => {
+router.post('/frontpage', async (req, res) => {
   try {
     const { FirstName, LastName, Email, PhoneNumber, Address } = req.body;
     const newSender = await Sender.create({ FirstName, LastName, Email, PhoneNumber, Address });
